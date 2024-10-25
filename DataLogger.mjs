d632d35eaabfcd8fc8dc649c8b7d1a4cdc52cf46
unknown <piwko@mail.com>
2024-10-26 00:53:21 +0200

    opis

---
+++ DataLogger.mjs
@@ -0,0 +1,45 @@
+import readline from 'node:readline';
+import fs from 'node:fs/promises';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let firstName, lastName, age;
+
+rl.question('podaj swoje imie: ', (answerFirstName) => {
+    firstName = answerFirstName;
+    rl.question('podaj swoje nazwisko: ', (answerLastName) => {
+        lastName = answerLastName;
+        rl.question('podaj swoj wiek: ', (answerAge) => {
+            age = answerAge;
+            rl.close();
+            saveToFile(); 
+        });
+    });
+});
+
+function saveToFile() {
+    const fileName = 'daneUzytkownak.json';
+
+    try {
+        fs.writeFileSync(fileName, `imie: ${firstName}, nazwisko: ${lastName}, wiek: ${age}`);
+        console.log(`Zapisano dane do pliku: ${fileName}`);
+        readFromFile();
+    } catch (error) {
+        console.error('cos poszlo nie tak :(', error);
+    }
+}
+
+
+
+function readFromFile() {
+    
+    try {
+        const data = fs.readFileSync('daneUzytkownak.json', 'utf-8');
+        console.log(data); 
+    } catch (error) {
+        console.error('blad podczas odczytu pliku:', error);
+    }
+}
